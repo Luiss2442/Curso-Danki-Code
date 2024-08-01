68dc7acb217cd5845709717ba96d24f566b53b88
Luis V Ratuchenei <149034028+Luiss2442@users.noreply.github.com>
2024-08-01 15:35:42 -0300

    Básico
    básico do java, aula 01.

---
+++ Curso_Danki_Code/src/basico.java
@@ -0,0 +1,103 @@
+public class basico {
+    
+    public static void main(String[] args) {
+        
+        /* 
+        System.out.println("Meu primeiro programa em java!");
+
+        int idade = 18;
+        System.out.println("Minha idade é "+idade);
+        idade = 19;
+        System.out.println("Ano que vem vou fazer "+idade);
+        */
+
+        //Tipos de varáveis
+        /* 
+        int numero = 24; //numeros inteiros
+        String frase = "plin plin plon"; //frases
+        boolean varia1 = true; //verdadeiro ou falso
+        char varia2 = 'a'; //uma letra, um caracter
+        double varia3 = 2.5; //numeros quebrados
+
+        System.out.println(numero);
+        System.out.println(frase);
+        System.out.println(varia1);
+        System.out.println(varia2);
+        System.out.println(varia3);
+        */
+
+        //Constantes
+        /* 
+        final int vida_maxima = 20;
+        
+        System.out.println(vida_maxima);
+        */
+
+        //Arrays
+        /* 
+        String[] nome = new String[5];
+        nome[0]= "Nathaly";
+        nome[1]= "Luis";
+        nome[2]= "Kira";
+        nome[3]= "Henry";
+        nome[4]= "Vó Nely";
+        */
+        //Array Multidimensional
+        /* 
+        String[][] varia2 = new String [3][3];
+        varia2[0][0] = "Gato";
+        varia2[0][1] = "Cachorro";
+        System.out.println(varia2[0][1]);
+        */
+
+        //Condições
+        /*
+        >
+        <
+        >=
+        <=
+        ==
+        !=
+        */
+        /*
+        int vida = 20;        
+        if(vida == 20){
+            vida++;
+            System.out.println("Minha vida é maior que 20!");
+        }else{
+            System.out.println("Minha vida não é igual a 20!");
+        }
+        
+        String nome = "Nathaly";
+        if(nome == "Luis"){
+            System.out.println("Nome igual a "+nome);
+        }else if(nome == "Nathaly"){
+            System.out.println("Nome igual a "+nome);
+        }else {
+            System.out.println("é a Kira");
+        }
+        
+        if(nome != "Kira"){
+            System.out.println("O nome é diferente de Kira");
+        }
+
+        boolean varia4 = false;
+        if(varia4){
+            System.out.println("Verdade");
+        }else {
+            System.out.println("Falso");
+        }
+        boolean varia5 = true;
+        if(varia5){
+            System.out.println("Verdade");
+        }else {
+            System.out.println("Falso");
+        }
+        */
+
+
+    }
+
+}
+
+
