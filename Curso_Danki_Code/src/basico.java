00fff465c370d6279970fbfd6b116b814c1fc20f
Luis V Ratuchenei <149034028+Luiss2442@users.noreply.github.com>
2024-08-02 15:27:45 -0300

    ajustes

--- Curso_Danki_Code/src/basico.java
+++ Curso_Danki_Code/src/basico.java
@@ -1,103 +1,177 @@
+
 public class basico {
+	
+	public static void main(String[] args) {
+
+	/* 
+    System.out.println("Meu primeiro programa em java!");
+
+    int idade = 18;
+    System.out.println("Minha idade é "+idade);
+    idade = 19;
+    System.out.println("Ano que vem vou fazer "+idade);
     
-    public static void main(String[] args) {
-        
-        /* 
-        System.out.println("Meu primeiro programa em java!");
 
-        int idade = 18;
-        System.out.println("Minha idade é "+idade);
-        idade = 19;
-        System.out.println("Ano que vem vou fazer "+idade);
-        */
+    //Tipos de varáveis
+    /* 
+    int numero = 24; //numeros inteiros
+    String frase = "plin plin plon"; //frases
+    boolean varia1 = true; //verdadeiro ou falso
+    char varia2 = 'a'; //uma letra, um caracter
+    double varia3 = 2.5; //numeros quebrados
 
-        //Tipos de varáveis
-        /* 
-        int numero = 24; //numeros inteiros
-        String frase = "plin plin plon"; //frases
-        boolean varia1 = true; //verdadeiro ou falso
-        char varia2 = 'a'; //uma letra, um caracter
-        double varia3 = 2.5; //numeros quebrados
+    System.out.println(numero);
+    System.out.println(frase);
+    System.out.println(varia1);
+    System.out.println(varia2);
+    System.out.println(varia3);
+    */
 
-        System.out.println(numero);
-        System.out.println(frase);
-        System.out.println(varia1);
-        System.out.println(varia2);
-        System.out.println(varia3);
-        */
+    //Constantes
+    /* 
+    final int vida_maxima = 20;
+    
+    System.out.println(vida_maxima);
+    */
 
-        //Constantes
-        /* 
-        final int vida_maxima = 20;
-        
-        System.out.println(vida_maxima);
-        */
+    //Arrays
+    /* 
+    String[] nome = new String[5];
+    nome[0]= "Nathaly";
+    nome[1]= "Luis";
+    nome[2]= "Kira";
+    nome[3]= "Henry";
+    nome[4]= "Vó Nely";
+    */
+    //Array Multidimensional
+    /* 
+    String[][] varia2 = new String [3][3];
+    varia2[0][0] = "Gato";
+    varia2[0][1] = "Cachorro";
+    System.out.println(varia2[0][1]);
+    */
 
-        //Arrays
-        /* 
-        String[] nome = new String[5];
-        nome[0]= "Nathaly";
-        nome[1]= "Luis";
-        nome[2]= "Kira";
-        nome[3]= "Henry";
-        nome[4]= "Vó Nely";
-        */
-        //Array Multidimensional
-        /* 
-        String[][] varia2 = new String [3][3];
-        varia2[0][0] = "Gato";
-        varia2[0][1] = "Cachorro";
-        System.out.println(varia2[0][1]);
-        */
-
-        //Condições
-        /*
-        >
-        <
-        >=
-        <=
-        ==
-        !=
-        */
-        /*
-        int vida = 20;        
-        if(vida == 20){
-            vida++;
-            System.out.println("Minha vida é maior que 20!");
-        }else{
-            System.out.println("Minha vida não é igual a 20!");
-        }
-        
-        String nome = "Nathaly";
-        if(nome == "Luis"){
-            System.out.println("Nome igual a "+nome);
-        }else if(nome == "Nathaly"){
-            System.out.println("Nome igual a "+nome);
-        }else {
-            System.out.println("é a Kira");
-        }
-        
-        if(nome != "Kira"){
-            System.out.println("O nome é diferente de Kira");
-        }
-
-        boolean varia4 = false;
-        if(varia4){
-            System.out.println("Verdade");
-        }else {
-            System.out.println("Falso");
-        }
-        boolean varia5 = true;
-        if(varia5){
-            System.out.println("Verdade");
-        }else {
-            System.out.println("Falso");
-        }
-        */
-
-
+    //Condições
+    /*
+    >
+    <
+    >=
+    <=
+    ==
+    !=
+    */
+    /*
+    int vida = 20;        
+    if(vida == 20){
+        vida++;
+        System.out.println("Minha vida é maior que 20!");
+    }else{
+        System.out.println("Minha vida não é igual a 20!");
+    }
+    
+    String nome = "Nathaly";
+    if(nome == "Luis"){
+        System.out.println("Nome igual a "+nome);
+    }else if(nome == "Nathaly"){
+        System.out.println("Nome igual a "+nome);
+    }else {
+        System.out.println("é a Kira");
+    }
+    
+    if(nome != "Kira"){
+        System.out.println("O nome é diferente de Kira");
     }
 
+    boolean varia4 = false;
+    if(varia4){
+        System.out.println("Verdade");
+    }else {
+        System.out.println("Falso");
+    }
+    boolean varia5 = true;
+    if(varia5){
+        System.out.println("Verdade");
+    }else {
+        System.out.println("Falso");
+    }
+    */
+	
+	//Switch e Case	
+	/*
+	int vida = 10;
+	
+	switch(vida) {
+	case 10:
+		//execute alguma ação aqui
+		System.out.println("boost de velocidade x2");
+		break;
+	case 5:
+		System.out.println("boost de dano x5");
+		break;
+		default:
+			System.out.println("permaneço de vida cheia");
+			break;
+	}
+	*/
+	
+	//Operações matematicas
+		/*
+		int vida_1 = 20;
+		int vida_2 = 20;
+		int vida_3 = 20;
+		int vida_final = vida_1 + vida_2 + vida_3;
+		
+		int vida_final2 = ((vida_1 + vida_2) /2) *2;
+		
+		System.out.println(vida_final2);
+		System.out.println(vida_final);
+		*/
+		/*
+		String nome_jogador = "Luis";
+		int skill = 100;
+		/*
+		if((nome_jogador == "Luis" && skill == 100) && skill <= 50) {
+			System.out.println("Verdade");
+		}else {
+			System.out.println("Entrou no else");
+		}
+		
+		if((nome_jogador == "Luis" || nome_jogador == "luis") && skill >= 100) {
+			System.out.println("Começe o jogo!");
+		}
+		*/
+		
+		//Loopings
+			/*
+			int contador = 0;
+			
+			while(contador < 11) {
+				System.out.println("Olá!, contador está em: "+contador);
+				contador++;
+			}
+			
+			for(int i =0; i<11; i+=2) {
+				System.out.println("contando: "+i);
+			}
+			*/
+			/*
+			contador = 0;
+			do {
+				System.out.println(contador);
+				contador++;
+			}while(contador < 11);
+			*/
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
-
+	
+}
 
